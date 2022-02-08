58adbbd077a1eb34042f607a4c57b29b2d1a2556
IvanL24 <lavenaivan@gmail.com>
2022-02-08 01:29:47 -0700

    remain logged in in home page when there is session still exists

--- MyLogin/src/main/java/ca/sait/mylogin/servlets/LoginServlet.java
+++ MyLogin/src/main/java/ca/sait/mylogin/servlets/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -27,7 +28,15 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        
+        if(session.getAttribute("username") != null){
+            response.sendRedirect("home");
+            return;
+        }
+        
         String query = request.getQueryString();
+        
         
         if (query != null && query.contains("logout")){
             
