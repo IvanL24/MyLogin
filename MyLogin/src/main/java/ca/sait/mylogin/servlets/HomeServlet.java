89a337cce8503de54859c8c8dbe07006732677bb
IvanL24 <lavenaivan@gmail.com>
2022-02-08 01:23:34 -0700

    Updated HomeServlet.java, username is now change to user's username when they logged in.

--- MyLogin/src/main/java/ca/sait/mylogin/servlets/HomeServlet.java
+++ MyLogin/src/main/java/ca/sait/mylogin/servlets/HomeServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -24,6 +25,11 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        
+        String username = (String) session.getAttribute("username");
+        
+        request.setAttribute("username", username);
         getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
     }
 
